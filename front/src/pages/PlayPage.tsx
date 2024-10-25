653e01824f318bf887657e262266d816c62ef99f
Riley Bruins <ribru17@hotmail.com>
2024-10-25 12:59:29 -0700

    feat: better card styling and functionality

--- front/src/pages/PlayPage.tsx
+++ front/src/pages/PlayPage.tsx
@@ -1,32 +1,96 @@
-import TinderCard from "react-tinder-card";
+import { Button, Container } from '@mui/material';
+import { createRef, useMemo, useRef, useState } from 'react';
+import TinderCard from 'react-tinder-card';
+import './PlayPage.css';
 
-const onSwipe = (direction: string) => {
-  console.log("You swiped: " + direction);
-};
+// TODO: Populate with data from the backend.
+const db = [
+  {
+    text: 'TEXT',
+  },
+  {
+    text: 'MORE TEXT',
+  },
+  {
+    text: 'HERE TEXT',
+  },
+  {
+    text: 'STUFF HERE',
+  },
+  {
+    text: 'TESTING STUFF',
+  },
+];
 
-const onCardLeftScreen = () => {
-  console.log("card left the screen");
-};
-
-// TODO: Polish this. See https://github.com/3DJakob/react-tinder-card-demo/blob/master/src/examples/Advanced.js
 function PlayPage() {
+  const [currentIndex, setCurrentIndex] = useState(db.length - 1);
+  const currentIndexRef = useRef(currentIndex);
+
+  const swiped = (_direction: string, index: number) => {
+    updateCurrentIndex(index - 1);
+  };
+
+  const outOfFrame = (idx: number) => {
+    console.log(
+      `card with index ${idx} left the screen!`,
+      currentIndexRef.current,
+    );
+    currentIndexRef.current >= idx && childRefs[idx].current.restoreCard();
+  };
+
+  const swipe = async (dir: string) => {
+    if (canSwipe && currentIndex < db.length) {
+      await childRefs[currentIndex].current.swipe(dir); // Swipe the card!
+    }
+  };
+  const childRefs = useMemo(
+    () =>
+      Array(db.length)
+        .fill(0)
+        .map((_) => createRef<any>()),
+    [],
+  );
+
+  const updateCurrentIndex = (val: number) => {
+    setCurrentIndex(val);
+    currentIndexRef.current = val;
+  };
+
+  const canSwipe = currentIndex >= 0;
+
   return (
-    <TinderCard
-      onSwipe={onSwipe}
-      onCardLeftScreen={onCardLeftScreen}
-      preventSwipe={["down", "up"]}
-    >
-      <div
-        style={{
-          height: 500,
-          backgroundImage:
-            "url(https://basepaws.com/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Fqj2yxv39d6ig%2F4XXFhU2WQtuORzIVqFvoay%2Fdda16ebb32a51db8a620aac1fb9ee5e7%2Fj45Rv.jpg%3Fw%3D755%26h%3D503%26q%3D80&w=1920&q=75)",
-        }}
-        className="card"
-      >
-        <h3>Hey</h3>
-      </div>
-    </TinderCard>
+    <Container>
+      <Container className='cardContainer'>
+        {db.map((card, index) => (
+          <TinderCard
+            onSwipe={(dir) => swiped(dir, index)}
+            onCardLeftScreen={() => outOfFrame(index)}
+            preventSwipe={['down', 'up']}
+            className='card'
+            ref={childRefs[index]}
+            key={index}
+          >
+            <Container className='innerCard'>
+              {card.text}
+            </Container>
+          </TinderCard>
+        ))}
+      </Container>
+      <Container className='buttons'>
+        <Button
+          style={{ backgroundColor: !canSwipe ? '#c3c4d3' : 'red' }}
+          onClick={() => swipe('left')}
+        >
+          Scam!
+        </Button>
+        <Button
+          style={{ backgroundColor: !canSwipe ? '#c3c4d3' : 'green' }}
+          onClick={() => swipe('right')}
+        >
+          Not a scam
+        </Button>
+      </Container>
+    </Container>
   );
 }
 
