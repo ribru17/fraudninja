35f1e8e204e7fb86228cf05f673e7c2b57afea32
Riley Bruins <ribru17@hotmail.com>
2024-11-16 19:21:00 -0800

    test: webpack tests

--- front/src/App.test.tsx
+++ front/src/App.test.tsx
@@ -1,8 +1,9 @@
+import { expect, test } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/If you don't already have an account, click/i);
   expect(linkElement).toBeInTheDocument();
 });
