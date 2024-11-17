b541b51ed15c58131e7b370de6a7365971dbbbaa
Riley Bruins <ribru17@hotmail.com>
2024-11-16 19:21:00 -0800

    test: vitest tests

---
+++ front/vitest.config.ts
@@ -0,0 +1,13 @@
+// vitest.config.ts
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  plugin: [react()],
+  test: {
+    globals: true, // allows us to use vitest library methods in unit test without explicit imports
+    environment: 'jsdom',
+    setupFiles: './setupTests.ts', // path to setup file
+  },
+});
