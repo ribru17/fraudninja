d84070e483a5a116db4cbffbd78cac93a5c92b63
annaellebgt <61291538+annaellebgt@users.noreply.github.com>
2024-11-25 17:33:48 -0800

    pop up when clicking on one resource (#78)
    * pop up when clicking on one resource
    
    * update max length display
    
    * Update front/src/components/ResourceDetail.tsx
    
    Co-authored-by: Riley Bruins <ribru17@hotmail.com>
    
    * Update front/src/components/ResourceBox.tsx
    
    Co-authored-by: Riley Bruins <ribru17@hotmail.com>
    
    * remove unused component
    
    * update import react with type
    
    * fix wrong import
    
    ---------
    
    Co-authored-by: Riley Bruins <ribru17@hotmail.com>

--- front/src/pages/ResourcePage.tsx
+++ front/src/pages/ResourcePage.tsx
@@ -1,18 +1,21 @@
 import { useEffect, useRef, useState } from 'react';
 import type React from 'react';
-import { Grid2 } from '@mui/material';
+import { Grid2, Modal } from '@mui/material';
 import ResourceBox from '../components/ResourceBox';
 import ContainerPage from '../components/ContainerPage';
 import type { Resource } from '@shared_types';
 import ApiSdk from '../api/apiSdk';
 import { useAppSelector } from '../redux/hook';
 import FullScreenSpinner from '../components/FullScreenSpinner';
+import ResourceDetail from '../components/ResourceDetail';
 
 const ResourcePage: React.FC = () => {
   const [resources, setResources] = useState<Resource[]>();
   const hasFetched = useRef(false);
   const api = new ApiSdk();
   const { token } = useAppSelector((state) => state.session);
+  const [openHelpModal, setOpenHelpModal] = useState<boolean>(false);
+  const [resourceSelected, setResourceSelected] = useState<Resource>();
 
   useEffect(() => {
     if (!hasFetched.current) {
@@ -25,6 +28,11 @@ const ResourcePage: React.FC = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [token]);
 
+  const openModalResource = (resource: Resource) => {
+    setResourceSelected(resource);
+    setOpenHelpModal(true);
+  };
+
   if (!resources) return <FullScreenSpinner />;
   return (
     <ContainerPage title='Scam Resources'>
@@ -34,11 +42,24 @@ const ResourcePage: React.FC = () => {
             <ResourceBox
               category={resource.category}
               content={resource.content}
-              links={resource.links}
               imageUrl={
                 resource.image ? `data:image/jpeg;base64,${resource.image}` : ''
               }
+              onClick={() => openModalResource(resource)}
             />
+            <Modal
+              open={openHelpModal}
+              onClose={() => setOpenHelpModal(false)}
+              slotProps={{
+                backdrop: {
+                  sx: {
+                    backgroundColor: 'rgba(256, 256, 256, 0.3)', // Semi-transparent black
+                  },
+                },
+              }}
+            >
+              <ResourceDetail resource={resourceSelected} />
+            </Modal>
           </Grid2>
         ))}
       </Grid2>
